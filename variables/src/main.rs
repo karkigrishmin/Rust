ba012f879a0be71836f3a5532d108f41bb35d2ca
Grishmin karki <grishmink@gmail.com>
2020-09-30 00:38:44 +0545

    basic concepts in variables and mutability

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,19 +1,26 @@
 fn main() {
     //mutable variable
-    // let mut x = 5;
+    let mut a = 5;
 
-    // println!("value of x is: {}", x);
+    println!("value of a is: {}", a);
 
-    // x = 6;
+    a = 6;
 
-    // println!("value of x is: {}", x)
+    println!("value of a is: {}", a);
 
     //Shadowing
+    //Shadowing and mut are different, we can specify type of second variable in shadowing
     let x = 5;
 
     let x = x + 1;
 
     let x = x * 3;
 
-    println!("Value of x is: {}", x)
+    println!("Value of x is: {}", x);
+
+    //----Constant-----, we have to specify type of the constant as well
+    //underscore and uppercase is the naming convention
+    const MAX_VALUE: u32 = 100_000;
+
+    println!("max value is: {}", MAX_VALUE)
 }
