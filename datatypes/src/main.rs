33b93ecd42c1ed6132b98adc9170dbcebd1ca8b6
Grishmin karki <grishmink@gmail.com>
2020-10-01 01:23:05 +0545

    Compound Type -> Tuple

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -15,5 +15,20 @@ fn main() {
     //Character Type
     let c = 'Z';
 
-    println!("Value of integer x is x: {},value of floating point y is y: {}, boolean a and b is {}, {} and character c is : {}  ", x, y, a, b, c)
+    println!("Value of integer x is x: {},value of floating point y is y: {}, boolean a and b is {}, {} and character c is : {}  ", x, y, a, b, c);
+
+    //Compound Types :- Tuple and array
+
+    //Tuple Type
+    let tup = (500, 5.5, 'G');
+
+    //Destructuring through pattern matching, to access the value of tuple
+    let (d, e, f) = tup;
+
+    println!("d: {}, e: {}, f: {} ", d, e, f);
+
+    //we can also access the tuple elements using '.' followed by index of value
+    let five_hundred = tup.0;
+
+    println!("{}", five_hundred);
 }
