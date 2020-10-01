8f3afe6efa2be77edb8247397798b5705b2d62bf
Grishmin karki <grishmink@gmail.com>
2020-10-01 22:58:56 +0545

    passing parameter to the function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(10);
 }
 
 //we define function using fn keyword and naming convention is snake case
-fn another_function() {
-    println!("Another function")
+fn another_function(x: i32) {
+    println!("{}", x)
 }
