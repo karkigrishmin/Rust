65d9fffaca0a029b11a0654af0ba24b190ead64e
Grishmin karki <grishmink@gmail.com>
2020-10-02 00:16:24 +0545

    function returning value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,24 @@
 fn main() {
     println!("Hello, world!");
 
+    let y = {
+        let a = 3;
+        a + 2
+    };
+
+    println!("{}", y);
+
     another_function(10);
+
+    //function return value
+    let x = plus_one(5);
+
+    println!("The value of x is: {}", x);
+}
+
+//function which return value -> 6
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
 
 //we define function using fn keyword and naming convention is snake case
