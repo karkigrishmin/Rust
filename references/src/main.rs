9e820796df90d1275f42b85d7d9bccdca97c47f0
Grishmin karki <grishmink@gmail.com>
2020-10-09 23:32:04 +0545

    mutable reference

--- references/src/main.rs
+++ references/src/main.rs
@@ -3,8 +3,25 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}", s1, len);
+
+    mutable_reference();
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+
+    
+}
+
+//we have  to use &mut to mutate reference value
+fn mutable_reference(){
+    let mut s1 = String::from("Grishmin");
+
+    change(&mut s1);
+
+    println!("{}", s1);
+}
+
+fn change(s:&mut String){
+    s.push_str(" karki");
 }
