a434b9caedb86e2d6557d56e1a318d21c77336fa
Grishmin karki <grishmink@gmail.com>
2020-10-19 23:54:05 +0545

    example using tuple

--- struct_rectangle/src/main.rs
+++ struct_rectangle/src/main.rs
@@ -6,8 +6,21 @@ fn main() {
         "The area of reactangle is {} square pixels.",
         area(width, height)
     );
+
+    // Example using tuple
+    using_tuple();
 }
 
 fn area(w: u32, h: u32) -> u32 {
     w * h
+}
+
+// Calculating area using tuple
+fn using_tuple() {
+    let rect = (20, 30);
+    println!("The area of rectangle is {}", area_using_tuple(rect));
+}
+
+fn area_using_tuple(reactangle: (u32, u32)) -> u32 {
+    reactangle.0 * reactangle.1
 }
